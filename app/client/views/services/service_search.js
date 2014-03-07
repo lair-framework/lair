1e9f24e8d31afcc40dbd32a8328eb54c521e8f89
tomsteele <thomasjsteele@gmail.com>
2014-03-07 00:05:15 -0800

    Fixes #49

--- app/client/views/services/service_search.js
+++ app/client/views/services/service_search.js
@@ -2,14 +2,44 @@
 // See the file license.txt for copying permission
 
 var hostIds = [];
+var reduced = false;
+var match = [];
+
+Template.serviceSearch.projectId = function() {
+  return Session.get('projectId');
+};
+
+Template.serviceSearch.servicesWithHosts = function() {
+  if (Session.equals('servicesViewQuery', null)) {
+    return null;
+  }
+  match.forEach(function(match) {
+    var host = Hosts.findOne({
+      "_id": match.host_id
+    });
+    match.string_addr = host.string_addr;
+    match.long_addr = host.long_addr;
+  });
+  return match.sort(sortLongAddr);
+};
 
 Template.serviceSearch.services = function() {
-  var match = null;
   if (Session.equals('servicesViewQuery', null)) {
-    match = Ports.find({"project_id": Session.get('projectId')}, {sort: {"port": 1, "product": 1}}).fetch();
-  }
-  else {
-    match = Ports.find(Session.get('servicesViewQuery'), {sort: {"port": 1, "product": 1}}).fetch();
+    match = Ports.find({
+      "project_id": Session.get('projectId')
+    }, {
+      sort: {
+        "port": 1,
+        "product": 1
+      }
+    }).fetch();
+  } else {
+    match = Ports.find(Session.get('servicesViewQuery'), {
+      sort: {
+        "port": 1,
+        "product": 1
+      }
+    }).fetch();
   }
   if (!match) {
     return {};
@@ -18,7 +48,12 @@ Template.serviceSearch.services = function() {
   hostIds = [];
   match.forEach(function(match) {
     hostIds.push(match.host_id);
-    services.push({"port": match.port, "protocol": match.protocol, "service": match.service, "product": match.product});
+    services.push({
+      "port": match.port,
+      "protocol": match.protocol,
+      "service": match.service,
+      "product": match.product
+    });
   });
   return unique(services);
 };
@@ -28,7 +63,15 @@ Template.serviceSearch.rendered = function() {
 };
 
 Template.serviceSearch.hosts = function() {
-  var hosts = Hosts.find({"_id": {"$in": _.uniq(hostIds)}}, {"sort": {"long_addr": 1}}).fetch();
+  var hosts = Hosts.find({
+    "_id": {
+      "$in": _.uniq(hostIds)
+    }
+  }, {
+    "sort": {
+      "long_addr": 1
+    }
+  }).fetch();
   return _.pluck(hosts, 'string_addr').join('\n');
 };
 
@@ -39,18 +82,29 @@ Template.serviceSearch.events({
     var protocol = escapeRegex(tpl.find('[name=protocol]').value);
     var service = escapeRegex(tpl.find('[name=service]').value);
     var product = escapeRegex(tpl.find('[name=product]').value);
-    var query = {"project_id": Session.get('projectId')};
+    var query = {
+      "project_id": Session.get('projectId')
+    };
     if (port && !isNaN(port)) {
       query.port = port;
     }
     if (protocol) {
-      query.protocol = { "$regex": protocol, "$options": 'i'};
+      query.protocol = {
+        "$regex": protocol,
+        "$options": 'i'
+      };
     }
     if (service) {
-      query.service = { "$regex": service, "$options": 'i'};
+      query.service = {
+        "$regex": service,
+        "$options": 'i'
+      };
     }
     if (product) {
-      query.product = { "$regex": product, "$options": 'i'};
+      query.product = {
+        "$regex": product,
+        "$options": 'i'
+      };
     }
     return Session.set('servicesViewQuery', query);
   },
@@ -60,8 +114,13 @@ Template.serviceSearch.events({
     var protocol = this.protocol;
     var service = this.service;
     var product = this.product;
-    var query = {"project_id": Session.get('projectId'), "port": port, "protocol": protocol,
-      "service": service, "product": product};
+    var query = {
+      "project_id": Session.get('projectId'),
+      "port": port,
+      "protocol": protocol,
+      "service": service,
+      "product": product
+    };
     tpl.find('[name=port]').value = port;
     tpl.find('[name=protocol]').value = protocol;
     tpl.find('[name=service]').value = service;
@@ -75,13 +134,29 @@ Template.serviceSearch.events({
     tpl.find('[name=service]').value = '';
     tpl.find('[name=product]').value = '';
     return Session.set('servicesViewQuery', null);
-  }
+  },
+
+  'click .port-status': function() {
+    var status = STATUS_MAP[STATUS_MAP.indexOf(this.status) + 1];
+    if (STATUS_MAP.indexOf(this.status) + 1 > 4) {
+      status = STATUS_MAP[0];
+    }
+    return Meteor.call('setPortStatus', Session.get('projectId'), this._id, status);
+  },
+
+  'click .flag-enabled': function() {
+    return Meteor.call('disablePortFlag', Session.get('projectId'), this._id);
+  },
+
+  'click .flag-disabled': function() {
+    return Meteor.call('enablePortFlag', Session.get('projectId'), this._id);
+  },
 
 });
 
 function unique(arr) {
   var hash = {}, result = [];
-  for (var i = 0, l = arr.length; i < l; ++i ) {
+  for (var i = 0, l = arr.length; i < l; ++i) {
     var oString = JSON.stringify(arr[i]);
     if (!hash.hasOwnProperty(oString)) {
       hash[oString] = true;
