ce7706202a250e6c9d6119547a1ce71a106ffd3d
Tom Steele <thomasjsteele@gmail.com>
2015-06-03 19:05:42 -0700

    Adds signout route

--- app/lib/router/signin.js
+++ app/lib/router/signin.js
@@ -1,5 +1,17 @@
-/* globals Router */
+/* globals Router Meteor */
 
 Router.route('/signin', {
-  name: 'signin'
+  name: 'signin',
+  onBeforeAction: function () {
+    Meteor.logout()
+    this.next()
+  }
+})
+
+Router.route('/signout', {
+  onBeforeAction: function () {
+    Meteor.logout()
+    this.redirect('signin')
+    this.next()
+  }
 })
