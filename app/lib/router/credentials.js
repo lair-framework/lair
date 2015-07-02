3696dffc087665c90b81cf535fac2bcd3ce1b837
Tom Steele <thomasjsteele@gmail.com>
2015-07-02 16:39:44 -0700

    Adds credentials router

---
+++ app/lib/router/credentials.js
@@ -0,0 +1,34 @@
+/* globals Router Credentials Projects */
+
+Router.route('/projects/:id/credentials', {
+  name: 'credentials',
+  controller: 'ProjectController',
+  data: function () {
+    if (Projects.find({
+        _id: this.params.id
+      }).count() < 1) {
+      return null
+    }
+    var self = this
+    return {
+      projectId: self.params.id,
+      credentials: Credentials.find({}).fetch()
+    }
+  }
+})
+
+Router.route('/projects/:id/credentials/new', {
+  name: 'newCredential',
+  controller: 'ProjectController',
+  data: function () {
+    if (Projects.find({
+        _id: this.params.id
+      }).count() < 1) {
+      return null
+    }
+    var self = this
+    return {
+      projectId: self.params.id
+    }
+  }
+})
